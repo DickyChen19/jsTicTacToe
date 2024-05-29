4f206d7f6a1f3582bc2aa776a35ea097b1b7f1bd
DickyChen19 <128550393+DickyChen19@users.noreply.github.com>
2024-05-29 14:09:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 var btns;
+var power;
 var xbtns =[];
 var obtns =[];
+var empty =[];
 var count = 0;
+var confirmed = 0;
+var ddosConfirmed = 0;
+var phishingConfirmed = 0;
+var firewallConfirmed = 0;
+var backupConfirmed = 0;
+var disabled = 0;
 
 function initialize() {
     var xbtns =[];
@@ -12,6 +20,13 @@ function initialize() {
     for (var i = 0; i < btns.length; i ++) {
         btns[i].addEventListener('click', function(i) {
             change(i);
+        }.bind(this, i));
+    }
+
+    power = document.querySelectorAll("button");
+    for (var i = 0; i < power.length; i ++) {
+        power[i].addEventListener('click', function(i) {
+            powers(i);
         }.bind(this, i));
     }
 }
@@ -29,6 +44,11 @@ function myfunc() {
         (btns[2].value == '0'))
         ) { 
         document.getElementById('print').innerHTML = `Player ${btns[0].value} wins`; 
+        for (let x of power) {
+            if (x.innerHTML != " RESET ") {
+                x.disabled = true;
+            }
+        }
         btns[3].disabled = true;
         btns[4].disabled = true;
         btns[5].disabled = true;
@@ -49,6 +69,11 @@ function myfunc() {
             (btns[6].value == '0'))
             ) {  
         document.getElementById('print').innerHTML = `Player ${btns[0].value} wins`; 
+        for (let x of power) {
+            if (x.innerHTML != " RESET ") {
+                x.disabled = true;
+            }
+        }
         btns[1].disabled = true;
         btns[2].disabled = true;
         btns[4].disabled = true;
@@ -69,6 +94,11 @@ function myfunc() {
             (btns[8].value == '0'))
             ) { 
         document.getElementById('print').innerHTML = `Player ${btns[6].value} wins`; 
+        for (let x of power) {
+            if (x.innerHTML != " RESET ") {
+                x.disabled = true;
+            }
+        }
         btns[0].disabled = true;
         btns[1].disabled = true;
         btns[2].disabled = true;
@@ -89,6 +119,11 @@ function myfunc() {
             (btns[8].value == '0'))
             ) { 
         document.getElementById('print').innerHTML = `Player ${btns[2].value} wins`; 
+        for (let x of power) {
+            if (x.innerHTML != " RESET ") {
+                x.disabled = true;
+            }
+        }
         btns[0].disabled = true;
         btns[1].disabled = true;
         btns[3].disabled = true;
@@ -109,6 +144,11 @@ function myfunc() {
             (btns[8].value == '0'))
             ) { 
         document.getElementById('print').innerHTML = `Player ${btns[0].value} wins`; 
+        for (let x of power) {
+            if (x.innerHTML != " RESET ") {
+                x.disabled = true;
+            }
+        }
         btns[1].disabled = true;
         btns[2].disabled = true;
         btns[3].disabled = true;
@@ -128,7 +168,12 @@ function myfunc() {
             (btns[4].value == '0') && 
             (btns[6].value == '0'))
             ) { 
-        document.getElementById('print').innerHTML = `Player ${btns[2].value} wins`;  
+        document.getElementById('print').innerHTML = `Player ${btns[2].value} wins`; 
+        for (let x of power) {
+            if (x.innerHTML != " RESET ") {
+                x.disabled = true;
+            }
+        } 
 
         btns[0].disabled = true;
         btns[1].disabled = true;
@@ -149,7 +194,12 @@ function myfunc() {
             (btns[4].value == '0') && 
             (btns[7].value == '0'))
             ) { 
-        document.getElementById('print').innerHTML = `Player ${btns[1].value} wins`; 
+        document.getElementById('print').innerHTML = `Player ${btns[1].value} wins`;
+        for (let x of power) {
+            if (x.innerHTML != " RESET ") {
+                x.disabled = true;
+            }
+        } 
         btns[0].disabled = true;
         btns[2].disabled = true;
         btns[3].disabled = true;
@@ -171,6 +221,11 @@ function myfunc() {
             (btns[5].value == '0'))
             ) { 
         document.getElementById('print').innerHTML = `Player ${btns[3].value} wins`; 
+        for (let x of power) {
+            if (x.innerHTML != " RESET ") {
+                x.disabled = true;
+            }
+        }
         btns[0].disabled = true;
         btns[1].disabled = true;
         btns[2].disabled = true;
@@ -197,6 +252,11 @@ function myfunc() {
         ) 
     { 
         document.getElementById('print').innerHTML = "Match Tie"; 
+        for (let x of power) {
+            if (x.innerHTML != " RESET ") {
+                x.disabled = true;
+            }
+        }
     } 
     else { 
   
@@ -221,18 +281,34 @@ flag = 1;
 
 function change(num) {
     if (flag == 1) {
+        if (disabled == 1) {
+            for (let x of empty) {
+                x.disabled = false;
+            }
+            disabled = 0;
+        }
         btns[num].value = "X";
         flag = 0;
         xbtns.push(btns[num]);
+        document.getElementById('print1').innerHTML = "";
+        document.getElementById('print2').innerHTML = "";
         if (xbtns.length > 3) {
             xbtns[0].value = "";
             xbtns[0].disabled = false;
             xbtns.shift();
         }
     } else if (flag == 0) {
+        if (disabled == 1) {
+            for (let x of empty) {
+                x.disabled = false;
+            }
+            disabled = 0;
+        }
         btns[num].value = "0";
         flag = 1;
         obtns.push(btns[num]);
+        document.getElementById('print1').innerHTML = "";
+        document.getElementById('print2').innerHTML = "";
         if (obtns.length > 3) {
             obtns[0].value = "";
             obtns[0].disabled = false;
@@ -241,4 +317,188 @@ function change(num) {
     }
     btns[num].disabled = true;
     myfunc();
-} 
+}
+
+function powers(num) {
+    empty = [];
+    if (flag == 1 && num == 0) {
+        if (ddosConfirmed == 0) {
+            document.getElementById('print1').innerHTML = `DDos: Randomly places 1-2 symbols`;
+            document.getElementById('print2').innerHTML = `Click again to activate`;
+            ddosConfirmed = 1;
+        } else if (ddosConfirmed == 1) {
+            for (let x of btns) {
+                if (x.value == "") {
+                    empty.push(x);
+                }
+            }
+            let y = Math.floor(Math.random() * empty.length);
+            empty[y].value = "X";
+            empty[y].disabled = true;
+            y = Math.floor(Math.random() * empty.length);
+            empty[y].value = "X";
+            empty[y].disabled = true;
+            power[num].disabled = true;
+            flag = 0;
+            ddosConfirmed = 0;
+            document.getElementById('print1').innerHTML = "";
+            document.getElementById('print2').innerHTML = "";
+            myfunc();
+        }
+    } else if (flag == 1 && num == 1) {
+        if (phishingConfirmed == 0) {
+            document.getElementById('print1').innerHTML = `Phishing: Randomly replaces one enemy symbol with your own`;
+            document.getElementById('print2').innerHTML = `Click again to activate`;
+            phishingConfirmed = 1;
+        } else if (phishingConfirmed == 1) {
+            for (let x of btns) {
+                if (x.value == "0") {
+                    empty.push(x);
+                }
+            }
+            if (empty.length == 0) {
+                phishingConfirmed = 0;
+                document.getElementById('print1').innerHTML = `Enemy has no placed symbols`;
+                document.getElementById('print2').innerHTML = "";
+            } else {
+                let y = Math.floor(Math.random() * empty.length);
+                empty[y].disabled = false;
+                empty[y].value = "X";
+                empty[y].disabled = true;
+                power[num].disabled = true;
+                flag = 0;
+                document.getElementById('print1').innerHTML = "";
+                document.getElementById('print2').innerHTML = "";
+                phishingConfirmed = 0;
+                myfunc();
+            }
+        }
+    } else if (flag == 1 && num == 2) {
+        if (firewallConfirmed == 0) {
+            document.getElementById('print1').innerHTML = `Firewall: Disables enemy's use of cards next turn`;
+            document.getElementById('print2').innerHTML = `Click again to activate`;
+            firewallConfirmed = 1;
+        } else if (firewallConfirmed == 1) {
+            for (let x = 4; x < 8; x ++) {
+                if (power[x].disabled != true) {
+                    empty.push(power[x]);
+                }
+                power[x].disabled = true;
+            }
+            disabled = 1;
+            power[num].disabled = true;
+            document.getElementById('print1').innerHTML = "";
+            document.getElementById('print2').innerHTML = "";
+            flag = 0;
+            firewallConfirmed = 0;
+            myfunc();
+        }
+    } else if (flag == 1 && num == 3) {
+        if (backupConfirmed == 0) {
+            document.getElementById('print1').innerHTML = `Data Backup: Resets the board to a blank state`;
+            document.getElementById('print2').innerHTML = `Click again to activate`;
+            backupConfirmed = 1;
+        } else if (backupConfirmed == 1) {
+            for (let x of btns) {
+                x.disabled = false;
+                x.value = "";
+            }
+            flag = 0;
+            power[num].disabled = true;
+            document.getElementById('print1').innerHTML = "";
+            document.getElementById('print2').innerHTML = "";
+            backupConfirmed = 0;
+            myfunc();
+        }
+    } else if (flag == 0 && num == 4) {
+        if (ddosConfirmed == 0) {
+            document.getElementById('print1').innerHTML = `DDos: Randomly places 1-2 symbols`;
+            document.getElementById('print2').innerHTML = `Click again to activate`;
+            ddosConfirmed = 1;
+        } else if (ddosConfirmed = 1) {
+            for (let x of btns) {
+                if (x.value == "") {
+                    empty.push(x);
+                }
+            }
+            let y = Math.floor(Math.random() * empty.length);
+            empty[y].value = "0";
+            empty[y].disabled = true;
+            y = Math.floor(Math.random() * empty.length);
+            empty[y].value = "0";
+            empty[y].disabled = true;
+            power[num].disabled = true;
+            flag = 1;
+            ddosConfirmed = 0;
+            document.getElementById('print1').innerHTML = "";
+            document.getElementById('print2').innerHTML = "";
+            myfunc();
+        }
+    } else if (flag == 0 && num == 5) {
+        if (phishingConfirmed == 0) {
+            document.getElementById('print1').innerHTML = `Phishing: Randomly replaces one enemy symbol with your own`;
+            document.getElementById('print2').innerHTML = `Click again to activate`;
+            phishingConfirmed = 1;
+        } else if (phishingConfirmed == 1) {
+            for (let x of btns) {
+                if (x.value == "X") {
+                    empty.push(x);
+                }
+            }
+            if (empty.length == 0) {
+                phishingConfirmed = 0;
+                document.getElementById('print1').innerHTML = `Enemy has no placed symbols`;
+                document.getElementById('print2').innerHTML = "";
+            } else {
+                let y = Math.floor(Math.random() * empty.length);
+                empty[y].disabled = false;
+                empty[y].value = "0";
+                empty[y].disabled = true;
+                power[num].disabled = true;
+                flag = 1;
+                document.getElementById('print1').innerHTML = "";
+                document.getElementById('print2').innerHTML = "";
+                phishingConfirmed = 0;
+                myfunc();
+            }
+        }
+    } else if (flag == 0 && num == 6) {
+        if (firewallConfirmed == 0) {
+            document.getElementById('print1').innerHTML = `Firewall: Disables enemy's use of cards next turn`;
+            document.getElementById('print2').innerHTML = `Click again to activate`;
+            firewallConfirmed = 1;
+        } else if (firewallConfirmed == 1) {
+            for (let x = 0; x < 4; x ++) {
+                if (power[x].disabled != true) {
+                    empty.push(power[x]);
+                }
+                power[x].disabled = true;
+            }
+            disabled = 1;
+            power[num].disabled = true;
+            document.getElementById('print1').innerHTML = "";
+            document.getElementById('print2').innerHTML = "";
+            flag = 1;
+            firewallConfirmed = 0;
+            myfunc();
+        }
+    } else if (flag == 0 && num == 7) {
+        if (backupConfirmed == 0) {
+            document.getElementById('print1').innerHTML = `Data Backup: Resets the board to a blank state`;
+            document.getElementById('print2').innerHTML = `Click again to activate`;
+            backupConfirmed = 1;
+        } else if (backupConfirmed == 1) {
+            for (let x of btns) {
+                x.disabled = false;
+                x.value = "";
+            }
+            flag = 1;
+            power[num].disabled = true;
+            document.getElementById('print1').innerHTML = "";
+            document.getElementById('print2').innerHTML = "";
+            backupConfirmed = 0;
+            myfunc();
+        }
+    }
+    
+}
